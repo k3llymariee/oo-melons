c03580abb9628fc36c9b8c2b04f4d2c7179844e9
Hackbright Student <no-reply@hackbrightacademy.com>
2019-10-11 12:41:02 -0700

    Move methods to AbstractMelonClass.

--- melons.py
+++ melons.py
@@ -7,43 +7,10 @@ class AbstractMelonOrder():
         self.qty = qty
         self.shipped = False
 
-
-class DomesticMelonOrder(AbstractMelonOrder):
-    """A melon order within the USA."""
-    order_type = "domestic"
-    tax = 0.08
-
     def get_total(self):
         """Calculate price, including tax."""
 
-        base_price = 5
-        total = (1 + self.tax) * self.qty * base_price
-
-        return total
-
-    def mark_shipped(self):
-        """Record the fact than an order has been shipped."""
-
-        self.shipped = True
-
-
-class InternationalMelonOrder(AbstractMelonOrder):
-    """An international (non-US) melon order."""
-
-    order_type = "international"
-    tax = 0.17
-
-    # def __init__(self, country_code):
-        # self.country_code = country_code
-
-    def __init__(self, species, qty, country_code):
-        super().__init__(species, qty)
-        self.country_code = country_code
-
-    def get_total(self):
-        """Calculate price, including tax."""
-
-        base_price = 5
+        base_price = 5 * 1.5         
         total = (1 + self.tax) * self.qty * base_price
 
         return total
@@ -57,3 +24,21 @@ class InternationalMelonOrder(AbstractMelonOrder):
         """Return the country code."""
 
         return self.country_code
+
+
+class DomesticMelonOrder(AbstractMelonOrder):
+    """A melon order within the USA."""
+    order_type = "domestic"
+    tax = 0.08
+    country_code = 'USA'
+
+
+class InternationalMelonOrder(AbstractMelonOrder):
+    """An international (non-US) melon order."""
+
+    order_type = "international"
+    tax = 0.17
+
+    def __init__(self, species, qty, country_code):
+        super().__init__(species, qty)
+        self.country_code = country_code
